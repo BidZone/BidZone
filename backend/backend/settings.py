81f913f034ac59290eb1287157b0b935fd67a590
Ante Goreta <agoret00@fesb.hr>
2024-10-12 15:53:49 +0200

    added bitcoin miner

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-&f=+deuyf3%q46bfhhhq-gjn$ulcrcpnv5#w%(9g7bs)&ki1bw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
