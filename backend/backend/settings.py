b59497ba9f36f0054303df2e3919edd6ac6c6128
Ante Goreta <agoret00@fesb.hr>
2024-10-12 20:09:54 +0200

    enviroment secret keys hidden

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&f=+deuyf3%q46bfhhhq-gjn$ulcrcpnv5#w%(9g7bs)&ki1bw'
+SECRET_KEY = env(‘SECRET_KEY’)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
