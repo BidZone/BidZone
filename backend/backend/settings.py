b8ee6781a66e5172712732b9e89ba6a2a263a3e3
Ante Goreta <agoret00@fesb.hr>
2024-10-12 15:49:59 +0200

    added 2 more domains

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bidzoneapi.onrender.com']
+ALLOWED_HOSTS = ['bidzoneapi.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
