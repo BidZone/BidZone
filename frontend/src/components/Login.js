d2a94bca3fad0acc3ab7cdacc6f954c3f14d0202
BogeticKarlo <147347211+BogeticKarlo@users.noreply.github.com>
2024-12-08 21:17:57 +0100

    Added login files and modified app.js

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Login.css'; // Import the CSS file
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ usernameOrEmail: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div>
+          <label>Username or Email</label>
+          <input
+            type="text"
+            name="usernameOrEmail"
+            value={credentials.usernameOrEmail}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
