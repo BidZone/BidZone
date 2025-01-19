3dfb7d68b3b68b1a78ae660622019e515f163463
BogeticKarlo <147347211+BogeticKarlo@users.noreply.github.com>
2025-01-19 18:16:39 +0100

    Vratio valjano stanje Navbara

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -174,7 +174,7 @@ const Navbar = () => {
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link" to="/profileOverview">
+                    <Link className="nav-link" to="/profile">
                       Profile
                     </Link>
                   </li>
